94918c8d5e5508c098a13c07a6ea5e581db741a7
brob <bryanlrobinson@gmail.com>
2022-10-12 14:13:31 -0400

    Stable version

--- src/extensions/Dialog.js
+++ src/extensions/Dialog.js
@@ -1,5 +1,5 @@
-import { BaukastenProvider, Button, DialogHeader, DialogContent, Input, Label, Stack, Inline, Flex, Select } from '@hygraph/baukasten';
-
+import { BaukastenProvider, Button, DialogHeader, DialogContent, Input, Label, Stack, Flex, Select } from '@hygraph/baukasten';
+import { useEffect } from 'react';
 // 2. import Inter font stylesheet
 
 
@@ -8,7 +8,14 @@ import {
   useFieldExtension,
 } from '@graphcms/uix-react-sdk';
 import React from 'react';
-import { Autocomplete } from './Autocomplete';
+
+
+const getSpells = async () => {
+  const response = await fetch('https://www.dnd5eapi.co/api/spells');
+  const data = await response.json();
+  return data.results;
+
+}
 
 
 export const DialogButton = () => {
@@ -24,23 +31,78 @@ export const DialogButton = () => {
   return (
     <BaukastenProvider>
       <Flex gap="16">
-        <Button size="large" onClick={handleOpenDialog}>Find ID</Button>
+        <Input flex="4" value={value} onChange={onChange} />
+        <Button flex="1" size="large" onClick={handleOpenDialog}>Find ID</Button>
       </Flex>
     </BaukastenProvider>
   )
 };
 
-
-export const Dialog = async () => {
-  const { onCloseDialog, value } = useUiExtensionDialog();
-  const [state, setState] = React.useState(value);
+export const Field = () => {
+  const { value } = useUiExtensionDialog();
+  const [state, setState] = React.useState(value || '');
+  const [spells, setSpells] = React.useState([]);
+  // useEffect to get spell list for select
+  useEffect(() => {
+    async function awaitSpells() {
+      const data = await getSpells()
+      const items = data.map(spell => {
+        return { label: spell.name, value: spell.index }
+      })
+      setSpells(items)
+    }
+    if (spells.length === 0) {
+      awaitSpells()
+    }
+  }, [spells])
 
   return (
-    <BaukastenProvider>
+      <BaukastenProvider>
+        <Stack gap="16">
+          <Select  items={spells} selectedItem={state} onChange={(e) => {
+            setState(e)
+          }} />
+        </Stack>
+      </BaukastenProvider>
+  );
+};
+
+
+export const Dialog = () => {
+  const { onCloseDialog, value } = useUiExtensionDialog();
+  const [state, setState] = React.useState(value || '');
+  const [spells, setSpells] = React.useState([]);
+  // useEffect to get spell list for select
+  useEffect(() => {
+    async function awaitSpells() {
+      const data = await getSpells()
+      const items = data.map(spell => {
+        return { label: spell.name, value: spell.index }
+      })
+      setSpells(items)
+    }
+    if (spells.length === 0) {
+      awaitSpells()
+    }
+  }, [spells])
+
+  console.log({spells, state})
+
+  return (
+      <BaukastenProvider>
 
       <DialogHeader>Find your D&D Spell</DialogHeader>
       
-
-    </BaukastenProvider>
+      <DialogContent>
+        <Stack gap="16">
+          <Label htmlFor="spell">Spell Key</Label>
+          <Select  items={spells} selectedItem={state} onChange={(e) => {
+            console.log(e)  
+            setState(e)
+          }} />
+          <Button alignSelf="end" size='large' onClick={() => onCloseDialog(state)}>Select</Button>
+        </Stack>
+      </DialogContent>
+      </BaukastenProvider>
   );
 };
